b35f7dedfdc8af4e0f4ebf6608d59ba7adec329d
NigarAdigozalova <72kwbg1@code.edu.az>
2022-11-11 11:08:57 +0400

    modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,17 @@
-let btn_click = document.getElementById("btn_click").onclick =function AddRow() {
+let btn_add = document.getElementById("btn_add").onclick =function () {
     
 
-    document.getElementById("table").style.display="block";
-    let firstName=document.getElementById("firstName").value
-    let lastName=document.getElementById("flastName").value
-    let old=document.getElementById("old").value
-
-
-   let list1=[0];
-   let list2=[0];
-   let list3=[0];
-
-   let x=0;
-   let n=1;
-   function AddRow(){
-    let AddRown=document.getElementById("table");
-    let NewRow=AddRown.insertRow(n);
-    
-    list1[x]=document.getElementById("firstName").value;
-    list2[x]=document.getElementById("lastName").value;
-    list3[x]=document.getElementById("old").value;
-
-    let cel1=NewRow.insertCell(0);
-    let cel2=NewRow.insertCell(1);
-    let cel3=NewRow.insertCell(2);
-
-    cel1.innerHTML=list1[x];
-    cel2.innerHTML=list2[x];
-    cel3.innerHTML=list3[x];
-
-    
-
-
-    
-
-   }
+    // document.getElementById("table").style.display="block";
+    let firstName=document.getElementById("name").value
+    let lastName=document.getElementById("lastName").value
+    let old=document.getElementById("age").value
    
-
-
-
+    let st_table=document.querySelector("tbody");
+    st_table.innerHTML+=`
+    <tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${old}</td>
+  </tr>
+    `
 }
